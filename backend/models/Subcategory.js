63b1f8126a9345396d9269c40146f454876e68b9
Valeriu Mazur <160285130+VALGFX@users.noreply.github.com>
2025-07-27 11:18:55 +0300

    Delete backend directory

--- backend/models/Subcategory.js
+++
@@ -1,9 +0,0 @@
-// models/Subcategory.js
-const mongoose = require('mongoose')
-
-const SubcategorySchema = new mongoose.Schema({
-	name: { type: String, required: true, unique: true },
-	// alte câmpuri dacă ai nevoie
-})
-
-module.exports = mongoose.model('Subcategory', SubcategorySchema)
--- backend/models/verificationCodeModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-
-const verificationCodeSchema = new mongoose.Schema({
-	email: { type: String, required: true, unique: true },
-	code: { type: String, required: true },
-	expireAt: { type: Number, required: true },
-})
-
-export default mongoose.model('VerificationCode', verificationCodeSchema)
